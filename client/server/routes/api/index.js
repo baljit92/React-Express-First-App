f652c944560620a55866bd0981c6b3b2e8218a03
Baljit Singh <baljit92@gmail.com>
2019-02-11 10:37:44 +0300

    Removed redundant files

--- client/server/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/users', require('./users'));
-router.use('/leaves', require('./leave'));
-module.exports = router;
